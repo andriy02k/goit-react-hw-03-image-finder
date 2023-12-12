ecc28d9511c7c325f4dd56977972968a1f1737ed
andriy02k <krainyk02@gmail.com>
2023-12-12 19:22:22 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react'
+import '../index.css';
+import Searchbar from './Searchbar/Searchbar'
+import ImageGallery from './ImageGallery/ImageGallery'
+import { getAllImages } from './api/getImages'
+import Loader from './Loader/Loader'
+import Modal from './Modal/Modal'
+import Button from './Button/Button';
+  
+export class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+    gallery: [],
+    isLoading: false,
+    largeImage: '',
+    showModal: false,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.page !== prevState.page || this.state.query !== prevState.query) {
+      this.getImages()
+    }
+  }
+
+  getImages = async () => {
+    try {
+      this.setState({ isLoading: true });
+      const responce = await getAllImages(this.state.query, this.state.page);
+      this.setState((prev) => ({
+        gallery: [...prev.gallery, ...responce.hits],
+      }));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  onSubmit = (q) => {
+    this.setState({ query: q, page: 1, gallery: [] });
+  }
+
+  openModal = (largeURL) => {
+    this.setState({
+      largeImage: largeURL,
+      showModal: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      largeImage: '',
+      showModal: false,
+    });
+  };
+
+  handleLoadMore = () => {
+		this.setState((prev) => ({ page: prev.page + 1 }))
+	}
+
+  render() {
+    const { gallery, isLoading, largeImage, showModal } = this.state;
+      return (
+          <div className='App'>
+            <Searchbar onSubmit={this.onSubmit} />
+            <ImageGallery gallery={gallery} openModal={this.openModal} />
+            {isLoading && <Loader />}
+            {showModal && <Modal largeImage={largeImage} closeModal={this.closeModal} />}
+            {gallery.length > 0 && <Button onClick={this.handleLoadMore} />}
+          </div>
+        );
+    };
+}
+
+
+
